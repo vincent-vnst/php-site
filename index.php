f32f12592cc8729919aba0b5166515520e166942
vincent-vnst <vincent.vanneste@univ-littoral.fr>
2024-12-27 18:02:30 +0100

    Version initiale du site

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='fr'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Accueil</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href='index.php'>Accueil</a></li>
+                <li><a href='personnes.php'>Personnes</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Accueil</h1>
+    </main>
+
+    <footer>
+        <h2>TP PHP</h2>
+    </footer>
+</body>
+
+</html>
---
+++ personnes.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang='fr'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Personnes</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href='index.php'>Accueil</a></li>
+                <li><a href='personnes.php'>Personnes</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Afficher</h1>
+
+        <ul>
+            <li>
+                <img src='photos/1_aule.gif' alt='photo Aule' />
+                Aule Lucie 45 ans
+            </li>
+            <li>
+                <img src='photos/2_bon.gif' alt='photo Bon' />
+                Bon Jean 65 ans
+            </li>
+            <li>
+                <img src='photos/3_ball.gif' alt='photo Ball' />
+                Ball Annie 24 ans
+            </li>
+            <li>
+                <img src='photos/4_desbois.gif' alt='photo Desbois' />
+                Desbois Robin 56 ans
+            </li>
+            <li>
+                <img src='photos/5_celer.gif' alt='photo Céler' />
+                Céler Jacques 17 ans
+            </li>
+            <li>
+                <img src='photos/6_nade.gif' alt='photo Nade' />
+                Nade Marie 72 ans
+            </li>
+            <li>
+                <img src='photos/7_sapin.gif' alt='photo Sapin' />
+                Sapin Noël 12 ans
+            </li>
+            <li>
+                <img src='photos/8_heau.gif' alt='photo Heau' />
+                Heau Charles 65 ans
+            </li>
+            <li>
+                <img src='photos/9_tation.gif' alt='photo Tation' />
+                Tation Félicie 34 ans
+            </li>
+            <li>
+                <img src='photos/10_reneaux.gif' alt='photo Reneaux' />
+                Reneaux Mégane 34 ans
+            </li>
+            <li>
+                <img src='photos/11_kiroule.gif' alt='photo Kiroule' />
+                Kiroule Pierre 63 ans
+            </li>
+            <li>
+                <img src='photos/12_deschamps.gif' alt='photo Deschamps' />
+                Deschamps Rose 23 ans
+            </li>
+        </ul>
+    </main>
+    <footer>
+        <h2>TP PHP</h2>
+    </footer>
+</body>
+
+</html>
