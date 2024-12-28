2c052380743d0f3dd6551fd1c88c0d273199e980
vincent-vnst <vincent.vanneste@univ-littoral.fr>
2024-12-28 11:45:54 +0100

    BDD - Personnes

--- personnes.php
+++ personnes.php
@@ -1,5 +1,13 @@
 <?php
 require 'bdd.php';
+// création de la requête
+$sql = 'SELECT * FROM personnes';
+// envoi de la requête et récupération du résultat
+$statement = $db->prepare($sql);
+$statement->execute();
+
+$listePersonnes = $statement->fetchAll();
+
 $page_title = 'Personnes';
 ?>
 <!DOCTYPE html>
@@ -12,7 +20,7 @@ $page_title = 'Personnes';
 
     <main>
         <h1>Afficher</h1>
-
+        <a href='create.php'><button>Ajouter</button></a>
         <ul>
             <?php
             foreach ($listePersonnes as $personne) {
@@ -24,6 +32,12 @@ $page_title = 'Personnes';
                     <?= $personne['nom'] ?>
                     <?= $personne['prenom'] ?>
                     <?= $personne['age'] ?> ans
+                    <a href='delete.php?id=<?= $personne['id'] ?>'>
+                        <img src='images/poubelle.png' alt='supprimer' />
+                    </a>
+                    <a href='update.php?id=<?= $personne['id'] ?>'>
+                        <img src='images/crayon.png' alt='modifier' />
+                    </a>
                 </li>
                 <?php
             }
